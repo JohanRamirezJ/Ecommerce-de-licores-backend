655bf83955635726983c4bc0ca30b000997bc2fc
Johan Camilo Ramirez Jimenez <johankmilo2006@gmail.com>
2026-05-20 16:56:50 -0500

    add CORS config

---
+++ Ecommerce/src/main/java/com/unilibre/vinos_licores/app/Ecommerce/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.unilibre.vinos_licores.app.Ecommerce;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:5173",
+                        "https://ecommerce-de-licores-frontend-p8h2ytfc0-johan-ramirezs-projects.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
